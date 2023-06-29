56676e886623f82d7af43388518d77eb938111ef
RomanRVV <rvachyov.roma@yandex.ru>
2023-06-29 11:18:34 +0300

    Reduce the number of queries to the database. Make commendation list a global constant

--- script.py
+++ script.py
@@ -4,9 +4,41 @@ import random
 import sys
 
 
+COMMENDATIONS = ["Молодец!",
+                 "Отлично!",
+                 "Хорошо!",
+                 "Гораздо лучше, чем я ожидал!",
+                 "Ты меня приятно удивил!",
+                 "Великолепно!", "Прекрасно!",
+                 "Ты меня очень обрадовал!",
+                 "Именно этого я давно ждал от тебя!",
+                 "Сказано здорово – просто и ясно!",
+                 "Ты, как всегда, точен!",
+                 "Очень хороший ответ!",
+                 "Талантливо!",
+                 "Ты сегодня прыгнул выше головы!",
+                 "Я поражен!",
+                 "Уже существенно лучше!",
+                 "Потрясающе!",
+                 "Замечательно!",
+                 "Прекрасное начало!",
+                 "Так держать!",
+                 "Ты на верном пути!",
+                 "Здорово!",
+                 "Это как раз то, что нужно!",
+                 "Я тобой горжусь!",
+                 "С каждым разом у тебя получается всё лучше!",
+                 "Мы с тобой не зря поработали!",
+                 "Я вижу, как ты стараешься!",
+                 "Ты растешь над собой!",
+                 "Ты многое сделал, я это вижу!",
+                 "Теперь у тебя точно все получится!"
+                 ]
+
+
 def find_schoolkid(schoolkid_name):
     try:
-        schoolkid = Schoolkid.objects.filter(full_name__contains=schoolkid_name).get()
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
         return schoolkid
     except Schoolkid.MultipleObjectsReturned:
         print(f'С именем {schoolkid_name}, есть сразу несколько учеников, пожалуйста, укажите полное ФИО')
@@ -18,10 +50,7 @@ def find_schoolkid(schoolkid_name):
 
 def fix_marks(schoolkid_name):
     schoolkid = find_schoolkid(schoolkid_name)
-    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-    for bad_mark in bad_marks:
-        bad_mark.points = 5
-        bad_mark.save()
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
 
 def remove_chastisements(schoolkid_name):
@@ -32,40 +61,10 @@ def remove_chastisements(schoolkid_name):
 
 def create_commendation(schoolkid_name, subject):
     schoolkid = find_schoolkid(schoolkid_name)
-    commendations = ["Молодец!",
-                     "Отлично!",
-                     "Хорошо!",
-                     "Гораздо лучше, чем я ожидал!",
-                     "Ты меня приятно удивил!",
-                     "Великолепно!", "Прекрасно!",
-                     "Ты меня очень обрадовал!",
-                     "Именно этого я давно ждал от тебя!",
-                     "Сказано здорово – просто и ясно!",
-                     "Ты, как всегда, точен!",
-                     "Очень хороший ответ!",
-                     "Талантливо!",
-                     "Ты сегодня прыгнул выше головы!",
-                     "Я поражен!",
-                     "Уже существенно лучше!",
-                     "Потрясающе!",
-                     "Замечательно!",
-                     "Прекрасное начало!",
-                     "Так держать!",
-                     "Ты на верном пути!",
-                     "Здорово!",
-                     "Это как раз то, что нужно!",
-                     "Я тобой горжусь!",
-                     "С каждым разом у тебя получается всё лучше!",
-                     "Мы с тобой не зря поработали!",
-                     "Я вижу, как ты стараешься!",
-                     "Ты растешь над собой!",
-                     "Ты многое сделал, я это вижу!",
-                     "Теперь у тебя точно все получится!"
-                     ]
     lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
                                    group_letter=schoolkid.group_letter,
                                    subject__title=subject).order_by("?").first()
-    commendation = random.choice(commendations)
+    commendation = random.choice(COMMENDATIONS)
     if lesson:
         Commendation.objects.create(text=commendation,
                                     created=lesson.date,
